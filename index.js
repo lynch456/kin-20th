0f7a3ea0a4ef7164ddef0bb1cda375592f3a14bc
Kim Ik-Soo <87025474+lynch456@users.noreply.github.com>
2022-12-16 10:05:25 +0900

    commit

--- index.js
+++ index.js
@@ -28,8 +28,16 @@ $(document).ready(() => {
     }
     if (scrollY >= 1100 && ww >= 1024) {
       $("#menu").removeClass("hide");
+      $(".intro-video").attr(
+        "src",
+        "https://campaign-cdn.pstatic.net/0/campaign/2022/09/kin-20th-anniversary/video/spot_mo.mp4"
+      );
     } else if (scrollY < 1100 || ww <= 1023) {
       $("#menu").addClass("hide");
+      $(".intro-video").attr(
+        "src",
+        "https://campaign-cdn.pstatic.net/0/campaign/2022/09/kin-20th-anniversary/video/spot_pc.mp4"
+      );
     }
 
     if (
